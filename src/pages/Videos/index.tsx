d06e8b3ccb598686655ad237f72a8b1d1137c31a
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-05-30 21:15:17 -0300

    Show Videos

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,9 +1,38 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import api from '../../services/Axios';
+
+interface IVideos {
+    id: string;
+    title: string;
+    description: string;
+    video: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
 const Videos: FC = () => {
+    const [videos, setVideos] = useState<IVideos[]>();
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await api.get('/video');
+
+            setVideos(data);
+        })(); 
+    }, []);
+
     return (
         <>
             <h1>Videos</h1>
+            
+            { videos? videos.map(video =>
+                <>
+                    <h1>{ video.title }</h1>
+                    <span>{ video.description }</span>
+                    <video src={`http://localhost:8081/video/${video.video}`}></video>
+                </>
+            )
+            : '' }
         </>
     )
 }
